36aaa72331071d5d81ba729d71e9342fdcaa3b7b
Aayushman Chulet <aayushmanchulet05@gmail.com>
2026-01-21 02:16:09 +0530

    added axios header

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,8 @@ dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 const index_1 = __importDefault(require("./routes/v1/index"));
 const cors_1 = __importDefault(require("cors"));
+const axios_1 = __importDefault(require("axios"));
+axios_1.default.defaults.headers.common['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ dotenv.config();
 import express, {Request, Response} from "express"
 import v1 from "./routes/v1/index"
 import cors from "cors"
+import axios from 'axios';
+
+
+axios.defaults.headers.common['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
 
 
 const app = express();
